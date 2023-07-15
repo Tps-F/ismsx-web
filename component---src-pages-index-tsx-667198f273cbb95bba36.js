6199180f7663d76cb2bef725783f68fcfecc9e42
Ftps <63702646+Tps-F@users.noreply.github.com>
2023-07-15 13:27:21 +0900

    Updates

---
+++ component---src-pages-index-tsx-667198f273cbb95bba36.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkisshin=self.webpackChunkisshin||[]).push([[691],{7200:function(e,n,t){t.r(n),t.d(n,{Head:function(){return s}});var l=t(7294);n.default=()=>l.createElement("h1",{className:"text-3xl font-bold underline"},"Hello world!");const s=()=>l.createElement("title",null,"Home Page")}}]);
+//# sourceMappingURL=component---src-pages-index-tsx-667198f273cbb95bba36.js.map
