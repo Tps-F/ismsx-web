42d48f8187881c328eff9753514b6c823e7d7818
Ftps <63702646+Tps-F@users.noreply.github.com>
2024-01-06 21:45:23 +0900

    works2

---
+++ src/components/worksGrid2.tsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from "react";
+import { database } from "../utils/database";
+import { sortByDate } from "../utils/sort";
+
+const WorksGrid = () => {
+	const [works, setWorks] = useState<any[]>([]);
+
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const { getWorks } = database();
+				const data = await getWorks();
+				setWorks(data.sort(sortByDate));
+			} catch (error) {
+				console.error("Error fetching works:", (error as Error).message);
+			}
+		};
+
+		fetchData();
+	}, []);
+
+	return (
+		<div className="grid gap-x-4 gap-y-6 sm:grid-cols-2 md:gap-x-6 lg:grid-cols-3 xl:grid-cols-4 ">
+			{works.map((work) => (
+				<div key={work.id}>
+					<a
+						href={work.url}
+						className="group mb-2 block w-full overflow-hidden rounded-lg bg-gray-100"
+					>
+						<img
+							src={`${process.env.GATSBY_SUPABASE_PROJECT_URL}/storage/v1/object/public/${work.image[0]}`}
+							alt={work.name}
+							className="h-full w-full object-cover object-center transition duration-200"
+						/>
+					</a>
+
+					<div className="flex flex-col">
+						<span className="text-gray-500">
+							{work.date.split("-").slice(0, 2).join("-")}
+						</span>
+						<a
+							href={work.url}
+							className="text-lg font-bold text-gray-800 transition duration-100 hover:text-gray-500 lg:text-xl"
+						>
+							{work.name}
+						</a>
+					</div>
+				</div>
+			))}
+		</div>
+	);
+};
+
+export default WorksGrid;
---
+++ src/pages/works2.tsx
@@ -0,0 +1,25 @@
+import type { HeadFC, PageProps } from "gatsby";
+import React from "react";
+
+import WorksGrid from "../components/worksGrid2";
+import Header from "../components/header";
+
+const Works2Page: React.FC<PageProps> = () => {
+	return (
+		<div className="min-h-screen bg-secondary py-6 sm:py-8 font-caviardreams">
+			<div className="mx-auto max-w-screen-2xl px-4 md:px-8">
+				<div className="mb-4 text-5xl font-bold sm:text-6xl md:mb-6 text-works grid-cols-2 grid">
+					<h1 className="mb-6 col-span-1 flex items-center">Works</h1>
+					<div className="mb-6 flex items-end justify-between gap-4 col-span-1">
+						<br />
+						<Header />
+					</div>
+				</div>
+				<WorksGrid />
+			</div>
+		</div>
+	);
+};
+
+export default Works2Page;
+export const Head: HeadFC = () => <title>isshin — Works</title>;
