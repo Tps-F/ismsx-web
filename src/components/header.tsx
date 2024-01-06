03b304328921308dae4fcfeb11a52fd55bdc7157
Ftps <63702646+Tps-F@users.noreply.github.com>
2024-01-07 02:58:42 +0900

    format

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { useLocation } from "@reach/router"
+import { useLocation } from "@reach/router";
 
 interface HeaderProps {
 	colors?: string[];
 }
 
 const Header: React.FC<HeaderProps> = ({ colors = [] }) => {
-	const location: string = useLocation().pathname.replace(/\//g, "")
+	const location: string = useLocation().pathname.replace(/\//g, "");
 	return (
 		<>
 			<div className="px-4 py-2 text-center text-sm font-semibold text-gray-500 outline-none ring-indigo-300 transition duration-100 hover:bg-gray-100 focus-visible:ring active:bg-gray-200 md:px-8 md:py-3 md:text-base">
