758350a63a2ee47c636c7541dddb93189fa0e45f
Ftps <63702646+Tps-F@users.noreply.github.com>
2024-01-07 23:18:09 +0900

    update works li

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -86,24 +86,162 @@ const AboutPage: React.FC<PageProps> = () => {
 					</h2>
 					<br />
 
-					<ul className="mb-6 list-inside list-disc text-gray-500 sm:text-lg md:mb-8">
-						<li>2023/06: 「ap bank fes ’23」ロゴモーション映像</li>
-						<li>2023/06: 新R25チャンネル「気持ち良い仕事睡眠大全」OP</li>
-						<li>2023/04: 株式会社うるる「ULU-Fes 2023」OP</li>
-						<li>2023/01: 日本テレビ「夜明け前のPLAYERS」OP</li>
-						<li>2022/12: スペースシャワーTVプラス OP</li>
-						<li>2022/11: スマ留「サクッと留学しよ。」PV映像</li>
-						<li>2022/07: 地域航空会社「feel air」PV映像</li>
-						<li>2022/05: MECRE「60秒でわかるMECRE」PV映像</li>
-						<li>2022/04: 株式会社うるる「ULU-Fes 2022」OP</li>
-						<li>2022/03: 「SQUARE ENIX MUSIC Channel」OP</li>
-						<li>2021/10: Twitterブランドセーフティ映像</li>
-						<li>2021/10: 株式会社うるる「ULURU Synapse Award 2021」OP</li>
+					<ul className="mb-6 list-inside text-gray-500 sm:text-lg md:mb-8">
+						<li>
+							2023/12　富士フィルム「クリスマスプレゼントにチェキ！」TVCM
+							モーション
+						</li>
+						<li>2023/10　「en Live Art Performance」一部演出</li>
+						<li>2023/10　Adobe「MAX Challenge 2023」参考作品</li>
+						<li>
+							2023/09　富士フィルム 「ワクワクがリンクする。INSTAX Pal」TVCM
+							モーション
+						</li>
+						<li>2023/09　株式会社ベストセキュリティ ロゴモーション</li>
+						<li>2023/08 「学び×楽しい 岩崎学園」PV</li>
+						<li>2023/06 「ap bank fes ’23 」ロゴモーション</li>
+						<li>2023/06　新R25チャンネル「気持ち良い仕事睡眠大全」OP</li>
+						<li>2023/05　DNP「BLUEPRINT STUDIO」"ONE KEY FOR EVERYTHING"</li>
+						<li>2023/04　株式会社うるる「ULU-Fes 2023」OP</li>
+						<li>2023/01　日本テレビ「夜明け前のPLAYERS」OP</li>
+						<li>2022/12　スペースシャワーTVプラス OP等</li>
+						<li>2022/11　スマ留 「サクッと留学しよ。」PV</li>
+						<li>2022/07　地域航空会社 「feel air」PV</li>
+						<li>2022/05　MECRE 「60秒でわかるMECRE」PV</li>
+						<li>2022/04　株式会社うるる「ULU-Fes 2022」OP</li>
+						<li>2022/03　「SQUARE ENIX MUSIC Channel」 OP等</li>
+						<li>2021/10　Twitter 「ブランドセーフティ」PV</li>
+						<li>2021/10　株式会社うるる「ULURU Synapse Award 2021」OP</li>
+						<li>2021/08　Microsoft 「Surface 4者面談」 OP他モーション</li>
+						<li>2021/07　「ﾐｭｰｼﾞｯｸﾚｲﾝｵｰﾃﾞｨｼｮﾝﾌﾟﾛｼﾞｪｸﾄ」 SPOT</li>
+						<li>2021/06　日立建機日本 ZX350LCK-6機能説明動画 一部演出</li>
+						<li>2021/06　GooglePlay 「Play On Challenge」 一部演出</li>
+						<li>2021/04　株式会社うるる「ULU-Fes 2021」OP</li>
+						<li>2021/03　KONAMI eFootballチャンネル OP・ED等</li>
+						<li>2021/02　株式会社SUPER STUDIO 「ecforce」ロゴモーション</li>
+						<li>2021/01　鳥取ガス「enetopia」TVCM 2カット</li>
+						<li>2021/01　株式会社リチカ サイトトップ映像　</li>
+						<li>2020/12　MUSIC ON! TV「legato 〜旅する音楽スタジオ〜」OP</li>
+						<li>2020/12　NHK みんなのうた「100年時代」アニメOP</li>
+						<li>2020/11　NewsPicksLIVE 「Westship」 PV / OP</li>
+						<li>2020/11　J:COMショップ吉祥寺店 サイネージ広告</li>
+						<li>2020/09　ヤマトグループ 「Yamato DX Night #1」 OP</li>
+						<li>2020/09　千葉ジェッツ 「10周年記念プロジェクト」 PV</li>
+						<li>2020/07　ポルカドットスティングレイ「FREE」 一部演出</li>
+						<li>2020/04　株式会社LABOT 「SHAKYO.io」 PV</li>
+						<li>2020/02　NHKスペシャル 「令和未来会議」OP</li>
+						<li>2020/01　justInCase株式会社 「わりかん保険」　PV</li>
+						<li>2019/11　YouTubeチャンネル 「STARDY」 　ロゴデザイン・PV</li>
+						<li>2019/09　ソニーミュージック「 One in a Billion」 　SPOT</li>
+						<li>2019/09　「THE BINARY」 　ライブ演出映像(一部)</li>
+						<li>2019/07　ASMR動画アプリ 「toremoro」 PV</li>
+						<li>2019/05　カクテルメイク株式会社 「RICHKA」 PV</li>
+						<li>2018/12　YouTubeチャンネル 「常春」 OP</li>
+						<li>2018/11　YouTubeチャンネル 「もやモヤちゃんねる」 OP</li>
+						<li>2018/10　「HoneyComeBear」　 ロゴモーション</li>
+						<li>2018/09　クリエイター交流イベント 「I/O」　 PV</li>
+						<li>2018/09　「ESPOIR表参道」　ロゴモーション</li>
+						<li>LECTURES </li>
+						<li>2021〜　bisen 北海道芸術デザイン専門学校にて特別講義</li>
+						<li>2021/06　放送芸術学院専門学校にて特別講義</li>
+						<li>SELF PRODUCED </li>
+						<li>2024/01　？？？？？</li>
+						<li>2023/10　"モーフィグレーション"</li>
+						<li>2023/05　"Dition"</li>
+						<li>2023/04　"Isshin REEL 2023"</li>
+						<li>2023/02　"nostalmic"</li>
+						<li>2022/11　"Voscot"</li>
+						<li>2022/06　"ミーレスラクト"</li>
+						<li>2022/04　"Sencotu"</li>
+						<li>2022/02　"知覚的融合"</li>
+						<li>2022/01　"Isshin REEL 2022" </li>
+						<li>2021/12　"Motete!“</li>
+						<li>2021/11　"Quiet or Upset”</li>
+						<li>2021/07　"Lipeatilca"</li>
+						<li>2021/07　"Estemovcion"</li>
+						<li>2021/05　"周回事象"</li>
+						<li>2021/04　"空間響図学"</li>
+						<li>2021/02　"Mars of sense"</li>
+						<li>2021/01　"Isshin REEL 2021" </li>
+						<li>2020/12　"20" </li>
+						<li>2020/10　"PASTREE"</li>
+						<li>2020/10　"Ceslot"</li>
+						<li>2020/09　"SECHAOS"</li>
+						<li>2020/08　“LA.PONZ”</li>
+						<li>2020/08　"monyu"</li>
+						<li>2020/07　"MOOOW"</li>
+						<li>2020/06　"jouet"</li>
+						<li>2020/06　"Resonat"</li>
+						<li>2020/05　"miscela"　</li>
+						<li>2020/04　"Fillanva"　</li>
+						<li>2020/04　"Hipnosis"　</li>
+						<li>2020/03　"Haptico"　</li>
+						<li>2020/02　"Anote"　</li>
+						<li>2020/02　"shift”　</li>
+						<li>2020/01　"アンノジョウオソイアケオメ”　</li>
+						<li>2020/01　"Isshin REEL 2020"　</li>
+						<li>2019/12　"Carei 19"</li>
+						<li>2019/12　"Platrap"</li>
+						<li>2019/10　"NAZO-01"</li>
+						<li>2019/08　"Planet"</li>
+						<li>2019/06　"BonNU"</li>
+						<li>2019/05　"ERA Update"</li>
+						<li>2019/04　"Reverse year"</li>
+						<li>2019/02　"NANKORE"</li>
+						<li>2019/02　"NyuNyun"</li>
+						<li>2019/01　"Funky Time"</li>
+						<li>2019/01　"メッチャオソイアケオメ"</li>
+						<li>2019/01　"Isshin REEL 2019"</li>
+						<li>2018/10　"Nine progression"</li>
+						<li>2018/09　文化祭企画映像</li>
+						<li>2018/08　"OTTF"</li>
+						<li>2018/07　"暑さで脳が死んだのよ"</li>
+						<li>2018/06　"期末試験マヂ無理"</li>
+						<li>2018/06　"ねこ様へ愛の言葉を。"</li>
+						<li>2018/06　"UZUDEZU"</li>
+						<li>2018/05　"INOUT"</li>
+						<li>2018/04　"PYON?“</li>
+						<li>2018/03　"connext!“</li>
+						<li>2018/02　"テスト死亡”</li>
+						<li>2018/02　"ROTATION" (EOMゲスト枠制作)</li>
+						<li>2018/01　"ISMSX REEL 2018"</li>
+						<li>2018/01　"2K+18"</li>
+						<li>2017/12　"LIMOV"</li>
+						<li>2017/10　"START ONE"</li>
+						<li>2017/10　"Arrest"</li>
+						<li>2017/10　"テスト詰んだ"</li>
+						<li>2017/08　"UNACCEPTABLE SPEED"</li>
+						<li>2017/08　"新たなモバイルOS" </li>
+						<li>2017/07　“QUOTE”</li>
+						<li>2017/06　"Hate MATH"</li>
+						<li>2017/05　"WHAT CAN I.“</li>
+						<li>2017/05　"AIRPORT GRAPHICS”</li>
+						<li>2017/04　"DATA Graphics"</li>
+						<li>2017/04　"EAST [ANOTHER City]“</li>
+						<li>2017/02　"WHI:GHT"</li>
+						<li>2016/12　"MO.6"</li>
+						<li>2016/09　文化祭課題制作</li>
+						<li>2016/07　"むーCATMOVIE ONE"</li>
+						<li>2016/07　"雷ーCATムービーWonder19"</li>
+						<li>2016/07　"Nature(Aichi)“</li>
+						<li>2016/05　"Hope will come true”</li>
+						<li>2016/05　"雷ーCAT ムービー ALONE18"</li>
+						<li>2016/04　"Let’s start.“</li>
+						<li>2016/02　"The 3-D’s last day.”　卒業ムービー制作　</li>
+						<li>2015/07　"Love the warz Real intention？”</li>
+						<li>2015/05　ゆず 「表裏一体」PV</li>
+						<li>2015/04　"Graphic Ri2"</li>
+						<li>2015/04　"Graphic Ri"</li>
+						<li>2015/03　じん 「夜咄ディセイブ」PV</li>
+						<li>2015/03　"2-D LASTs"</li>
+						<li>2015/03　"Graphic XC.o"</li>
+						<li>2015/03　"ISMA OS 9"</li>
+						<li>2015/03　"Graphic XA”</li>
+						<li>2015/02　"Dragon Night Graphic"</li>
+						<li>2015/02　"Memories of 2-D"</li>
+						<li>2014/11　クラス企画制作</li>
+						<li>2014/10　"MOON"</li>
 					</ul>
-
-					<blockquote className="mb-6 border-l-4 pl-4 italic text-gray-500 sm:text-lg md:mb-8 md:pl-6">
-						“リスト配置にしてあるけど、何もなしがよければ戻すよー！”
-					</blockquote>
 				</div>
 			</div>
 		</>
