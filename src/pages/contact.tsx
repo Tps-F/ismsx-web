e41a4d9d0115ec35c4093ef42cd162e70d4781ad
Ftps <63702646+Tps-F@users.noreply.github.com>
2024-01-13 19:48:33 +0900

    fix submittedState

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -118,7 +118,7 @@ const ContactPage: React.FC<PageProps> = () => {
 							</button>
 						</div>
 					</form>
-					{!submitted && (
+					{submitted && (
 						<div className="fixed top-20 left-0  w-full h-full bg-secondary py-6 sm:py-8 lg:py-12">
 							<div className="mx-auto max-w-screen-2xl px-4 md:px-8">
 								<div className="flex flex-col items-center">
